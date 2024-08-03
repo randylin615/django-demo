a24b43d9e0dd58b8d1074099eec1a9f1be05e86d
Randyyoo <randylin615@gmail.com>
2024-08-03 14:00:56 +0800

    new app: HelloDjangoApp urls.py default view change

--- django-demo/HelloDjangoApp/tests.py
+++ django-demo/HelloDjangoApp/tests.py
@@ -13,11 +13,11 @@ from django.test import TestCase
 class SimpleTest(TestCase):
     """Tests for the application views."""
 
-    if django.VERSION[:2] >= (1, 7):
-        # Django 1.7 requires an explicit setup() when running tests in PTVS
-        @classmethod
-        def setUpClass(cls):
-            django.setup()
+    # Django requires an explicit setup() when running tests in PTVS
+    @classmethod
+    def setUpClass(cls):
+        super(SimpleTest, cls).setUpClass()
+        django.setup()
 
     def test_basic_addition(self):
         """
--- django-demo/django_demo/urls.py
+++ django-demo/django_demo/urls.py
@@ -3,28 +3,32 @@ Definition of urls for django_demo.
 """
 
 from datetime import datetime
-from django.urls import path
+from django.urls import path, include, re_path
 from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
-from app import forms, views
+#from app import forms, views
+from HelloDjangoApp import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about'),
-    path('login/',
-         LoginView.as_view
-         (
-             template_name='app/login.html',
-             authentication_form=forms.BootstrapAuthenticationForm,
-             extra_context=
-             {
-                 'title': 'Log in',
-                 'year' : datetime.now().year,
-             }
-         ),
-         name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
-    path('admin/', admin.site.urls),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^home$', views.index, name='home'),
+
+    path('', views.index, name='home'),
+    #path('contact/', views.contact, name='contact'),
+    #path('about/', views.about, name='about'),
+    #path('login/',
+    #     LoginView.as_view
+    #     (
+    #         template_name='app/login.html',
+    #         authentication_form=forms.BootstrapAuthenticationForm,
+    #         extra_context=
+    #         {
+    #             'title': 'Log in',
+    #             'year' : datetime.now().year,
+    #         }
+    #     ),
+    #     name='login'),
+    #path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    #path('admin/', admin.site.urls),
 ]
