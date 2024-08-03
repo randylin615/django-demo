dc4fe3bfe5798760bf84708064a7eda7992969be
Randyyoo <randylin615@gmail.com>
2024-08-03 15:54:03 +0800

    implement of session 2

--- django-demo/HelloDjangoApp/views.py
+++ django-demo/HelloDjangoApp/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
-def index(request):
-    return HttpResponse("Hello, Django!")
-# Create your views here.
+#def index(request): ## ed.1
+#    now = datetime.now()
+
+    
+#    #html_content = "<html><head><title>Hello, Django</title></head><body>"
+#    #html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+#    #html_content += "</body></html>"
+#    #return HttpResponse(html_content)
+
+#    return render(
+#        request,
+#        "HelloDjangoApp/index.html",
+#        {
+#            'content':"<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+#        }
+#    )
+
+def index(request): ## ed.2
+    now = datetime.now()
+    return render(
+        request,
+        "HelloDjangoApp/index.html",
+        {
+            'title': "Hello Django",
+            'message': "Demo, Hello Django! ",
+            'content': " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
